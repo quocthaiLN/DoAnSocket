a1838da60d48697b58188b6d5dc640cb046e360a
thangquang2912 <168805280+thangquang2912@users.noreply.github.com>
2024-11-29 20:05:05 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,30 +1,49 @@
 import socket
 
-#Khai bao HOST, PORT
-HOST = socket.gethostname();
-PORT = 12000
+def uploadFile(client):
+    msg = input("Sever: Nhap vao ten file hoac duong dan file: ")
+    client.sendall(msg.encode('utf-8'))
+    resp = client.recv(1024)
+    resp = resp.decode('utf-8')
+    if resp == "Failed":
+        print("Sever: Tai file len that bai")
+    if resp == "Success":
+        print("Sever: Tai file len thanh cong")
+
+def downloadFile(client):
+    msg = input("Sever: Nhap vao ten file hoac duong dan file: ")
+    client.sendall(msg.encode('utf-8'))
+    resp = client.recv(1024)
+    resp = resp.decode('utf-8')
+    if resp == "Failed":
+        print("Sever: Tai file ve that bai")
+    if resp == "Success":
+        print("Sever: Tai file ve thanh cong")
+
+def menu():
+    print("0. Exit")
+    print("1. Download File")
+    print("2. Upload File")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    client.connect((HOST, PORT))#Ket noi vs Server
-    print("Connection to Server has been established!")
-    msg = ""
-    while True:
-        msg = input("Client: ")#Data ma Client muon gui
-        client.sendall(msg.encode('utf-8'))
-
-        if msg == "exit":#Neu Client muon exit
-            data = ""
-            data = client.recv(1024)
-            data = data.decode("utf-8")
-            print(f"Server: {data}")
-            msg = input("Your decision(Y/N): ")
+    client.connect((socket.gethostname(), 2810))
+    print("Ket noi thanh cong voi sever!!!")
+    while 1:
+        menu()
+        choice = int(input("nhap vao lua chon cua ban: "))
+        if choice == 0:
+            msg = "exit"
             client.sendall(msg.encode('utf-8'))
-            if msg == "Y":
-                print("You just disconnected from the Server.")
-                break
-            else:
-                continue   
+            break
+        if choice == 1:
+            msg = "downloadFile"
+            client.sendall(msg.encode('utf-8'))
+            downloadFile(client)
+        if choice == 2:
+            msg = "uploadFile"
+            client.sendall(msg.encode('utf-8'))
+            uploadFile(client)
 except:
     print("Connect error")
 client.close()
