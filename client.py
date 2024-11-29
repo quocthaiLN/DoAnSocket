7cc2d4c06e3f67588d46893a2847f41a4257aaf5
quocthaiLN <quocthailenguyen41@gmail.com>
2024-11-29 16:37:47 +0700

    update qthai1

--- client.py
+++ client.py
@@ -1,26 +1,30 @@
 import socket
 
+#Khai bao HOST, PORT
 HOST = socket.gethostname();
 PORT = 12000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    client.connect((HOST, PORT))
+    client.connect((HOST, PORT))#Ket noi vs Server
     print("Connection to Server has been established!")
     msg = ""
-    while msg != "exit":
-        msg = input("Client: ")
+    while True:
+        msg = input("Client: ")#Data ma Client muon gui
         client.sendall(msg.encode('utf-8'))
 
-        
-    # respFromSever = ""
-    # tmpAddr = client.getsockname()
-    # while respFromSever != "stop" :
-    #     respFromSever = client.recv(1024)
-    #     respFromSever = respFromSever.decode('utf-8')
-    #     if respFromSever == "stop":
-    #         break
-    #     print(f"Sever: {respFromSever}")
+        if msg == "exit":#Neu Client muon exit
+            data = ""
+            data = client.recv(1024)
+            data = data.decode("utf-8")
+            print(f"Server: {data}")
+            msg = input("Your decision(Y/N): ")
+            client.sendall(msg.encode('utf-8'))
+            if msg == "Y":
+                print("You just disconnected from the Server.")
+                break
+            else:
+                continue   
 except:
     print("Connect error")
 client.close()
--- server.py
+++ server.py
@@ -1,29 +1,50 @@
 import socket
 import threading
+import time
 
-HOST = socket.gethostname();
+#Khai bao HOST, PORT
+HOST = socket.gethostname()
 PORT = 12000
 
+#Ham ket noi voi client
 def recvData(client, addr) :
-    print(f"Connection from {addr} has been established!")
+    print(f"Connection from {addr} has been established!") #Thong bao da ket noi voi client
     try:
-        data = ""
-        while data != "exit":
-            data = client.recv(1024)
-            data = data.decode('utf-8')
-            if data == "exit":
-                break
-            print(f"Client {addr}: {data}")
-        if(data == "exit"):
-            client.send(bytes("Disconnected from the server."))
-            client.close()
-        # resp = ""
-        # while resp != "stop" :
-        #     resp = input("Sever: ")
-        #     client.sendall(resp.encode('utf-8'))
+        data = ""#Du lieu nhan duoc tu client
+        while True:
+            client.settimeout(600)#Dat thoi gian timeout la 10 phut
+            try:
+                
+                data = client.recv(1024)#Nhan data tu client
+                
+
+                data = data.decode('utf-8')#Decode data nhan duoc
+              
+                if(data == "exit"):#Neu client yeu cau exit
+                    print(f"Client {addr}: wants to disconect.")#Thong bao voi Server la Client muon exit
+                    client.send(bytes("Are you sure you want to disconnect?(Y/N)", "utf-8"))#Server gui thong bao cho Client, m chac chua
+                    data = client.recv(1024)#Nhan phan hoi tu Client
+                    data = data.decode('utf-8')
+                    if(data == "Y"):#Neu phan hoi la Y thi ngat ket noi, nguoc laij thi tiep tuc gia tiep
+                        client.close()
+                        print(f"Client {addr}: has been disconnected.")
+                        break
+                    else:
+                        print(f"Client {addr}: does not want to disconect.")
+                    
+                else:
+                    print(f"Client {addr}: {data}")#Neu khong exit thi in data ma Client gui ra
+            except socket.timeout:#Neu het thoi gian timeout ma khong co giao tiep nao duoc thuc hien thi bao loi va ngat ket noi
+                 client.close()
+                 print(f"Client {addr}: is not responding for too long.")
+                 break
+                
     except:
         print("Connect Error")
-    client.close()
+
+
+
+
 
 sever = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sever.bind((HOST, PORT))
@@ -31,7 +52,7 @@ NumsOfClient = int(input("The number of client: "))
 sever.listen(NumsOfClient)
 print("Waiting for connection.....")
 count = 0
-while count <= NumsOfClient:
+while count < NumsOfClient:
     client, addr = sever.accept()
     thread = threading.Thread(target = recvData, args = (client, addr))
     thread.daemon = False
