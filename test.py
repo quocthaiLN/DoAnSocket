d89112d0ee30149eed7a45fdfd8ff69558ff53b3
thangquang2912 <tranguyenquangthang2810@gmail.com>
2024-12-04 03:06:57 +0700

    ktrFolderRong

--- test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-# Lấy thời gian thực dưới dạng timestamp
-timestamp = time.time()
-print("Timestamp:", timestamp)
-
-# Chuyển thành thời gian định dạng
-formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
-print("Thời gian định dạng:", formatted_time)
