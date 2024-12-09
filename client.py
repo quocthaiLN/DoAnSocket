2991b5d441cc2d048d55f8b181c07de4c8865025
quocthaiLN <166308828+quocthaiLN@users.noreply.github.com>
2024-12-09 17:04:13 +0700

    Update client.py

--- client.py
+++ client.py
@@ -6,24 +6,24 @@ import time
 
 PathClient = "DataClient"
 
-#lay tu ki tu '/' cuoi cung tro ve sau trong duong dan hoac ten file
+#lay tu ki tu "/" cuoi cung tro ve sau trong duong dan hoac ten file
 def name(fileName):
     res = ""
     cnt = 0
     for chara in fileName:
-        if chara == '/':
+        if chara == "/":
             cnt += 1
     i = 0
     for charac in fileName:
-        if charac == '/' and i < cnt:
+        if charac == "/" and i < cnt:
             i += 1
         if i == cnt:
             res += charac
     return res
 
-#kiem tra xem co ki tu '/' trong ham o tren chua
+#kiem tra xem co ki tu "/" trong ham o tren chua
 def check(s):
-    if s[0] == '/':
+    if s[0] == "/":
         return True
     return False
 
@@ -56,28 +56,30 @@ PORT = 12000
 
 def uploadFile(client, msg):
     size = os.path.getsize(msg)
-    client.sendall(str(size).encode('utf-8'))
-    sizeResp = client.recv(1024).decode('utf-8')
+    client.sendall(str(size).encode("utf-8"))
+    sizeResp = client.recv(10240).decode("utf-8")
     
-    ifs = open(msg, "rb")
-    while 1:
-        data = ifs.read(1024)
-        if not data:
-            break
-        try:
-            client.sendall(data)
-        except Exception as e:
-            print(f"Co loi khi upload file {msg}/ Connect Error with sever.")
-            return False
-        try:
-            resp = client.recv(1024).decode('utf-8')
-        except Exception as e:
-            print(f"Co loi khi upload file {msg}/ Connect Error with sever.")
-            return False
-    ifs.close()
-    respSta = client.recv(1024).decode('utf-8')
-    client.sendall("da nhan".encode('utf-8'))
-    resp = client.recv(1024).decode('utf-8')
+    #ifs = open(msg, "rb")
+    with open(msg, "rb") as ifs:
+        while 1:
+            data = ifs.read(10240)
+            if not data:
+                break
+            try:
+                client.sendall(data)
+            except Exception as e:
+                print(f"Co loi khi upload file {msg}/ Connect Error with sever.")
+                return False
+            try:
+                resp = client.recv(10240).decode("utf-8")
+            except Exception as e:
+                print(f"Co loi khi upload file {msg}/ Connect Error with sever.")
+                return False
+    #ifs.close()
+    client.sendall("xong".encode("utf-8"))
+    respSta = client.recv(10240).decode("utf-8")
+    client.sendall("da nhan".encode("utf-8"))
+    resp = client.recv(10240).decode("utf-8")
     if resp == "Success":
         print(f"Sever: Da upload file {msg} len sever thanh cong. {respSta}")
     else:
@@ -85,30 +87,30 @@ def uploadFile(client, msg):
 
 def downloadFile(client):
     #Nhan yeu cau nhap duong dan tu sever
-    resp = client.recv(1024).decode('utf-8')
+    resp = client.recv(10240).decode("utf-8")
     print("Nhap CANCEL de thoat!!!")
     #nhap vao ten file
     msg = input(f"(Sever request) - {resp}")
-    if msg == 'CANCEL':
-        client.sendall(msg.encode('utf-8'))
+    if msg == "CANCEL":
+        client.sendall(msg.encode("utf-8"))
         return
     #gui ten file hoac duong dan
-    client.sendall(msg.encode('utf-8'))
+    client.sendall(msg.encode("utf-8"))
     #gui trang thai xem file co ton tai tren sever hay khong
-    checkStatus = client.recv(1024).decode('utf-8')
-    if checkStatus == 'ff':
+    checkStatus = client.recv(10240).decode("utf-8")
+    if checkStatus == "ff":
         print("File is in list forbidden file. Can't download this file")
         return
-    if checkStatus == 'Not exist':
+    if checkStatus == "Not exist":
         print("File khong ton tai!!!")
         return
     else:
         resp = "Da nhan duoc"
-        client.sendall(resp.encode('utf-8'))
+        client.sendall(resp.encode("utf-8"))
     #tao duong dan den noi luu tru file
     tmp = name(msg)
     if not check(tmp):
-        tmp = '/' + tmp
+        tmp = "/" + tmp
     nameWithNotExten = getNameWithNotExten(tmp)
     exten = getExten(tmp)
     fileWrite = PathClient + tmp
@@ -118,15 +120,15 @@ def downloadFile(client):
             tmp = nameWithNotExten + "(" + str(i) + ")" + exten
             fileWrite = PathClient + tmp
             i += 1
-    sizeRecv = client.recv(1024).decode('utf-8')
+    sizeRecv = client.recv(10240).decode("utf-8")
     size = int(sizeRecv)
     sizeResp = "Nhan thanh cong"
-    client.sendall(sizeResp.encode('utf-8'))
+    client.sendall(sizeResp.encode("utf-8"))
     ofs = open(fileWrite, "wb")
     sw = 0
     while size > sw:
         try:
-            data = client.recv(1024)
+            data = client.recv(10240)
         except Exception as e:
             print(f"Co loi khi download file {msg}/ Connect Error with sever.")
             return False
@@ -134,9 +136,13 @@ def downloadFile(client):
             break
         ofs.write(data)
         sw += len(data)
-        client.sendall(str(sw).encode('utf-8'))
+        try:
+            client.sendall(str(sw).encode("utf-8"))
+        except:
+            print(f"Co loi khi download file {msg}/ Connect Error with sever.")
+            return False
     ofs.close()
-    resp = client.recv(1024).decode('utf-8')
+    resp = client.recv(10240).decode("utf-8")
     if resp == "Success":
         print(f"Sever: Da download file thanh cong. File dang duoc luu tru tai {fileWrite} ")
     else:
@@ -144,17 +150,17 @@ def downloadFile(client):
 
 def uploadFilesInFolderSequentially(client, msg):
     fileName = os.listdir(msg)
-    client.sendall(str(len(fileName)).encode('utf-8'))
-    sizeResp = client.recv(1024).decode('utf-8')
+    client.sendall(str(len(fileName)).encode("utf-8"))
+    sizeResp = client.recv(10240).decode("utf-8")
     if sizeResp != "da nhan":
         print(f"Sever: {sizeResp}")
         return
     for file in fileName:
-        client.sendall(file.encode('utf-8'))
-        fileRep = client.recv(1024).decode('utf-8')
+        client.sendall(file.encode("utf-8"))
+        fileRep = client.recv(10240).decode("utf-8")
         uploadFile(client, msg + "/" + file)
-    client.sendall("da xong".encode('utf-8'))
-    res = client.recv(1024).decode('utf-8')
+    client.sendall("da xong".encode("utf-8"))
+    res = client.recv(10240).decode("utf-8")
     print(res)
 
 def menu():
@@ -165,7 +171,7 @@ def menu():
     
 def client_send(client, data):
     try:
-        client.sendall(data.encode('utf-8'))
+        client.sendall(data.encode("utf-8"))
     except socket.error: #BẮT LỖI TIME-OUT, SERVER TỰ NGẮT KẾT NỐI BẰNG CLOSE()
         print(f"Server: Đã ngắt kết nối.")
         client.close()
@@ -179,7 +185,7 @@ def client_send(client, data):
 
 def client_receive(client):
     try:
-        data = client.recv(1024).decode('utf-8')
+        data = client.recv(10240).decode("utf-8")
         if not data:
             print("Server đã đóng kết nối.")
             client.close()
@@ -201,7 +207,7 @@ def client_receive(client):
 
 def login(client):
     status = client_receive(client)
-    os.system('cls')
+    os.system("cls")
     print(status)
     res = client_send(client, "da nhan")
     reply = client_receive(client)
@@ -215,7 +221,7 @@ def login(client):
     # Nhận phản hồi từ server
     resp = client_receive(client)
     if resp == "Successful":
-        #os.system('cls')
+        #os.system("cls")
         print("Login Successful")
         return True
     else:
@@ -242,7 +248,7 @@ def main():
                     msg = "exit"
                     again_check = input("Ban co chac rang muon ngat ket noi chu?(Y/N): ")
                     if again_check == "Y":
-                        client.sendall(msg.encode('utf-8'))
+                        client.sendall(msg.encode("utf-8"))
                         print("Ban da ngat ket noi khoi Server.")
                         connect = False
                     else:
@@ -250,21 +256,21 @@ def main():
                 case 1:
                     flag = True
                     msg = "uploadFile"
-                    client.sendall(msg.encode('utf-8'))
+                    client.sendall(msg.encode("utf-8"))
                     #Nhan yeu cau nhap duong dan tu sever
-                    resp = client.recv(1024).decode('utf-8')
+                    resp = client.recv(10240).decode("utf-8")
                     while 1:
                         print("Nhap CANCEL de thoat!!!")
                         msg = input(f"(Sever request) - {resp}")
-                        if msg == 'CANCEL':
-                            client.sendall(msg.encode('utf-8'))
+                        if msg == "CANCEL":
+                            client.sendall(msg.encode("utf-8"))
                             Bin = client_receive(client)
                             flag = False
                             break
                         if not checkExist(msg):
                             print("File khong ton tai. Yeu cau nhap lai!!!")
                             continue
-                        client.sendall(msg.encode('utf-8'))
+                        client.sendall(msg.encode("utf-8"))
                         Bin = client_receive(client)
                         break
                     if flag == True:
@@ -272,26 +278,26 @@ def main():
 
                 case 2:
                     msg = "downloadFile"
-                    client.sendall(msg.encode('utf-8'))
+                    client.sendall(msg.encode("utf-8"))
                     downloadFile(client)
                 case 3:
                     msg = "uploadFilesInFolderSequentially"
-                    client.sendall(msg.encode('utf-8'))
+                    client.sendall(msg.encode("utf-8"))
                     flag = True
                     #Nhan yeu cau nhap duong dan folder tu sever
-                    resp = client.recv(1024).decode('utf-8')
+                    resp = client.recv(10240).decode("utf-8")
                     while 1:
                         print("Nhap CANCEL de thoat!!!")
                         msg = input(f"(Sever request) - {resp}")
-                        if msg == 'CANCEL':
-                            client.sendall(msg.encode('utf-8'))
+                        if msg == "CANCEL":
+                            client.sendall(msg.encode("utf-8"))
                             Bin = client_receive(client)
                             flag = False
                             break
                         if not checkFolderExist(msg):
                             print("Folder khong ton tai. Yeu cau nhap lai!!!")
                             continue
-                        client.sendall(msg.encode('utf-8'))
+                        client.sendall(msg.encode("utf-8"))
                         Bin = client_receive(client)
                         break
                     if flag == True:
